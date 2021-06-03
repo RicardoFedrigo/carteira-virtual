c4d7ae0b37566d83089e41970d0f4fdbdc47cb27
Ricardo Fedrigo <https://RicardoFedrigo@github.com>
2021-06-03 02:36:09 -0300

    Configuracao de notocao

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current'
-          }
-        }
-      ],
-      '@babel/preset-typescript'
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
     ],
-    plugins: [
-      ['module-resolver']
-    ],
-    ignore: [
-      '**/*.test.ts'
-    ]
-  }
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["module-resolver"],
+  ],
+  ignore: ["**/*.test.ts"],
+};
